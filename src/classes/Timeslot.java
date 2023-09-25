14b122529bf1d62453cd4bf64b84c4a6b7c9ac56
adhit612 <adhit612@gmail.com>
2023-09-25 18:57:17 -0400

    Added checklist to each class, added .equals() to some classes, mainly implemented Event class equals() and Date class all methods but needs testing

--- src/classes/Timeslot.java
+++ src/classes/Timeslot.java
@@ -1,4 +1,24 @@
 package classes;
 
 public enum Timeslot {
+
+    MORNING(10,30),
+    AFTERNOON(2,0),
+    EVENING(6,30);
+
+    private final int hours;
+    private final int mins;
+
+    Timeslot(int hours, int mins){
+        this.hours = hours;
+        this.mins = mins;
+    }
+
+    private int getHours(){
+        return hours;
+    }
+
+    private int getMins(){
+        return mins;
+    }
 }
